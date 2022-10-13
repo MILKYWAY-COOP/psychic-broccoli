31b85d86555e9e6a27aa0144626cc2bff9392963
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-13 14:05:10 +0300

    Updates

---
+++ src/Firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcvxa-EZ0Okb-6rGB9CRiAldUFvWvWnwQ",
+  authDomain: "movies-f9e60.firebaseapp.com",
+  projectId: "movies-f9e60",
+  storageBucket: "movies-f9e60.appspot.com",
+  messagingSenderId: "479765146124",
+  appId: "1:479765146124:web:542b340f315cdf009e9c29"
+};
+
+
+const app = initializeApp(firebaseConfig);
