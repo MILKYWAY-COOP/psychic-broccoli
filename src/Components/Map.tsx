6e2b207dd870a2916ee9d1ac747cef7ce25173ed
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-24 20:00:42 +0300

    Movie

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,23 +1,38 @@
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { googleKey } from '../auth';
+import { useMemo } from 'react';
+import { MapContainer } from './Styles/Map.styled';
 
-export const Map = () => {
-  //   let map: google.maps.Map;
+// create interface for the props
+interface MapProps {
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+}
 
-  //   function initMap(): void {
-  //     map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  //       center: { lat: -34.397, lng: 150.644 },
-  //       zoom: 8
-  //     });
-  //   }
+export default function MapComponent({ coordinates }: MapProps) {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: googleKey
+  });
 
-  //   declare global {
-  //     interface Window {
-  //       initMap: () => void;
-  //     }
-  //   }
-  //   window.initMap = initMap;
-  //   export {};
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map coordinates={coordinates} />;
+}
 
-  return <div id='map'>Map</div>;
-};
-
-export default Map;
+function Map({ coordinates }: MapProps) {
+  const { lat, lng } = coordinates;
+  console.log(lat, lng);
+  const center = useMemo(() => ({ lng: lng, lat: lat }), []);
+  return (
+    <MapContainer>
+      <GoogleMap
+        zoom={15}
+        center={center}
+        mapContainerClassName='map-container'
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </MapContainer>
+  );
+}
