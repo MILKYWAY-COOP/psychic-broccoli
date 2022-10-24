1ad52b4c2412294a4d4bf36cf898b76457b56b97
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-24 20:00:54 +0300

    Movie

--- src/Components/Styles/Map.styled.ts
+++ src/Components/Styles/Map.styled.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const MapContainer = styled.div``;
+export const MapContainer = styled.div`
+  .map-container {
+    background-color: aquamarine;
+    height: 100%;
+    width: 100%;
+  }
+`;
