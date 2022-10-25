87d324d9a2d1b300ae8ddd427720da7d6f421258
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-25 03:05:18 +0300

    Late night

--- src/Components/Styles/Map.styled.ts
+++ src/Components/Styles/Map.styled.ts
@@ -3,7 +3,34 @@ import styled from 'styled-components';
 export const MapContainer = styled.div`
   .map-container {
     background-color: aquamarine;
-    height: 100%;
-    width: 100%;
+    width: 800px;
+    height: 400px;
+    border-radius: 10px;
+    z-index: 9999;
+
+    @media (max-width: 800px) {
+      width: 700px;
+      height: 300px;
+    }
+
+    @media (max-width: 700px) {
+      width: 600px;
+      height: 300px;
+    }
+
+    @media (max-width: 600px) {
+      width: 500px;
+      height: 300px;
+    }
+
+    @media (max-width: 500px) {
+      width: 400px;
+      height: 300px;
+    }
+
+    @media (max-width: 400px) {
+      width: 300px;
+      height: 300px;
+    }
   }
 `;
