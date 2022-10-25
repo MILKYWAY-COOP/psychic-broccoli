1d55ef3734b73e2f3dba45991740bfd863195d29
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-25 03:04:44 +0300

    Late night

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,80 @@
 import { useEffect, useState } from 'react';
-import { Movies } from './Components';
+import { Movies, Welcome, Loading } from './Components';
 import { ninjasKey, imbdKey } from './auth';
 import './App.css';
 
 export default function App() {
-  const [place, setPlace] = useState('Nairobi');
+  const [place, setPlace] = useState<string>('');
+  const [isError, setIsError] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
-  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [loaded, setLoaded] = useState(false);
+  const [coordinates, setCoordinates] = useState({ lat: 10, lng: 10 });
+  const [movies, setMovies] = useState<any[]>([]);
 
-  // const GetLocation = async () => {
-  //   // if we were already searching for a location, don't do it again
-  //   if (isLoading) return;
+  useEffect(() => {
+    const getInfo = async () => {
+      if (isLoading || place === '') return;
+      setIsLoading(true);
+      const locationData = await fetch(
+        `https://api.api-ninjas.com/v1/city?name=${place}`,
+        {
+          method: 'GET',
+          headers: {
+            'X-Api-Key': ninjasKey
+          }
+        }
+      );
+      const dataJson = await locationData.json();
+      if (dataJson?.length === 0) {
+        setIsError(true);
+        setIsLoading(false);
+        setLoaded(false);
+        return;
+      }
 
-  //   // set the loading state to true
-  //   setIsLoading(true);
-  //   const locationData = await fetch(
-  //     `https://api.api-ninjas.com/v1/city?name=${place}`,
-  //     {
-  //       method: 'GET',
-  //       headers: {
-  //         'X-Api-Key': ninjasKey
-  //       }
-  //     }
-  //   );
+      setCoordinates({
+        lat: dataJson[0]?.latitude,
+        lng: dataJson[0]?.longitude
+      });
 
-  //   const dataJson = await locationData.json();
+      const movieData = await fetch(
+        `https://imdb-api.com/API/AdvancedSearch/${imbdKey}?locations=${place}`
+      );
+      const movieDataJson = await movieData.json();
+      setMovies(movieDataJson.results);
 
-  //   setCoordinates({
-  //     lat: dataJson[0]?.latitude,
-  //     lng: dataJson[0]?.longitude
-  //   });
+      if (movieDataJson?.results?.length === 0) {
+        setIsError(true);
+      } else {
+        setIsError(false);
+      }
+      setIsLoading(false);
+      setLoaded(true);
+    };
+    getInfo();
+  }, [place]);
 
-  //   const movieData = await fetch(
-  //     `https://imdb-api.com/API/AdvancedSearch/${imbdKey}?locations=${place}`
-  //   );
-  //   const movieDataJson = await movieData.json();
-  //   console.log(movieDataJson);
-  //   setIsLoading(false);
-  // };
-
-  // useEffect(() => {
-  //   GetLocation();
-  // }, []);
-
-  console.log(place);
-
-  return (
-    <div className='App'>
-      <Movies coordinates={coordinates} place={place} />
-      {/* <Welcome setPlace={setPlace} /> */}
-    </div>
-  );
+  if (isLoading && loaded === false) {
+    return <Loading />;
+  } else {
+    return (
+      <div className='App'>
+        {loaded ? (
+          <Movies
+            coordinates={coordinates}
+            place={place}
+            movies={movies}
+            setLoaded={setLoaded}
+          />
+        ) : (
+          <Welcome
+            setPlace={setPlace}
+            isError={isError}
+            place={place}
+            isLoading={isLoading}
+          />
+        )}
+      </div>
+    );
+  }
 }
