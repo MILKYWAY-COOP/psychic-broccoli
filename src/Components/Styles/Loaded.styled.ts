57057b493ad80e3ada2ea1c21e5626a810c35f1b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-26 21:54:12 +0300

    bugs

--- src/Components/Styles/Loaded.styled.ts
+++ src/Components/Styles/Loaded.styled.ts
@@ -1,155 +1,41 @@
 import styled from 'styled-components';
 
 export const LoadedContainer = styled.div`
-  display: flex;
-  align-items: center;
-  height: 100vh;
-  background-color: #001524;
-
-  .container {
-    display: block;
-    height: 300px;
-    width: 300px;
-    margin: 0 auto;
-  }
-  .baton {
-    display: block;
-    height: 2px;
-    width: 70px;
-    background-color: #459fa5;
-    animation: scale 1.25s infinite linear;
-    -webkit-transform-origin: 0;
-    -moz-transform-origin: 0;
-    -ms-transform-origin: 0;
-    -o-transform-origin: 0;
-    transform-origin: 0;
-    -webkit-transform: rotate(-10deg);
-    -moz-transform: rotate(-10deg);
-    -ms-transform: rotate(-10deg);
-    -o-transform: rotate(-10deg);
-    transform: rotate(-10deg);
-    &:before {
-      content: '';
-      display: block;
-      height: 5px;
-      width: 5px;
-      background-color: #f5a51c;
-      position: absolute;
-      top: -2px;
-      -webkit-border-radius: 5px;
-      -moz-border-radius: 5px;
-      -ms-border-radius: 5px;
-      -o-border-radius: 5px;
-      border-radius: 5px;
-    }
-    &:after {
-      content: '';
-      display: block;
-      height: 5px;
-      width: 5px;
-      background-color: #f5a51c;
-      position: absolute;
-      top: -2px;
-      right: 0;
-      -webkit-border-radius: 5px;
-      -moz-border-radius: 5px;
-      -ms-border-radius: 5px;
-      -o-border-radius: 5px;
-      border-radius: 5px;
-    }
+  .loader {
+    background-color: rgb(51,0,51);
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    z-index: 100000;
   }
 
-  .metronome {
-    -webkit-transform-origin: 0;
-    -moz-transform-origin: 0;
-    -ms-transform-origin: 0;
-    -o-transform-origin: 0;
-    transform-origin: 0;
-    animation: metronome 1.25s infinite linear;
+  .loader__element {
+    border-radius: 100%;
+    border: 20px solid white;
+    margin: 10px;
   }
 
-  @for $i from 0 through 35 {
-    .baton-#{$i} {
-      height: 1px;
-      width: 150px;
-      position: absolute;
-      top: calc(50% - 8px);
-      -webkit-transform-origin: 100%;
-      -moz-transform-origin: 100%;
-      -ms-transform-origin: 100%;
-      -o-transform-origin: 100%;
-      transform-origin: 100%;
-      -webkit-transform: rotate($i * 10deg);
-      -moz-transform: rotate($i * 10deg);
-      -ms-transform: rotate($i * 10deg);
-      -o-transform: rotate($i * 10deg);
-      .baton,
-      .baton:after,
-      .metronome {
-        animation-delay: -$i * 0.14s;
-      }
-    }
+  .loader__element:nth-child(1) {
+    animation: preloader 0.6s ease-in-out alternate infinite;
+  }
+  .loader__element:nth-child(2) {
+    animation: preloader 0.6s ease-in-out alternate 0.2s infinite;
   }
 
-  @keyframes metronome {
-    0% {
-      -webkit-transform: rotate(-25deg);
-      -moz-transform: rotate(-25deg);
-      -ms-transform: rotate(-25deg);
-      -o-transform: rotate(-25deg);
-      transform: rotate(-25deg);
-    }
-    50% {
-      -webkit-transform: rotate(25deg);
-      -moz-transform: rotate(25deg);
-      -ms-transform: rotate(25deg);
-      -o-transform: rotate(25deg);
-      transform: rotate(25deg);
-    }
+  .loader__element:nth-child(3) {
+    animation: preloader 0.6s ease-in-out alternate 0.4s infinite;
+  }
+
+  @keyframes preloader {
     100% {
-      -webkit-transform: rotate(-25deg);
-      -moz-transform: rotate(-25deg);
-      -ms-transform: rotate(-25deg);
-      -o-transform: rotate(-25deg);
-      transform: rotate(-25deg);
-    }
-  }
-
-  @keyframes scale {
-    0% {
-      -webkit-transform: scaleX(1);
-      -moz-transform: scaleX(1);
-      -ms-transform: scaleX(1);
-      -o-transform: scaleX(1);
-      transform: scaleX(1);
-    }
-    25% {
-      -webkit-transform: scaleX(0.74);
-      -moz-transform: scaleX(0.74);
-      -ms-transform: scaleX(0.74);
-      -o-transform: scaleX(0.74);
-      transform: scaleX(0.74);
-    }
-    50% {
-      -webkit-transform: scaleX(1);
-      -moz-transform: scaleX(1);
-      -ms-transform: scaleX(1);
-      -o-transform: scaleX(1);
-      transform: scaleX(1);
-    }
-    75% {
-      -webkit-transform: scaleX(1.16);
-      -moz-transform: scaleX(1.16);
-      -ms-transform: scaleX(1.16);
-      -o-transform: scaleX(1.16);
-      transform: scaleX(1.16);
-    }
-    100% {
-      -webkit-transform: scaleX(1);
-      -moz-transform: scaleX(1);
-      -ms-transform: scaleX(1);
-      -o-transform: scaleX(1);
-      transform: scaleX(1);
+      transform: scale(2);
     }
   }
 `;
